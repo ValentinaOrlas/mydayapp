94f499e35972800e7532f982e172caa0d2941ddb
ValentinaOrlas <johanavop25@gmail.com>
2024-10-23 04:16:27 -0500

    Ocultar la sección main y footer realizadas, terminadas y agregué icon en la pestaña

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -3,31 +3,28 @@ import { taskContext } from '../Context/Context';
 import Tarea from "../Tarea/Tarea";
 import Footer from "../Footer/Footer";
 import CrearTarea from "../CraerTarea/CrearTarea";
-import { useLocation } from "react-router-dom";
 
 const Info = () => {
   const context = useContext(taskContext);
-  const location = useLocation();
-
-  const filter = location.pathname.substring(1); 
   
-  const filteredTasks = context.tasks.filter(task => {
-    if (filter === "completed") return task.status === "completed";
-    if (filter === "pending") return task.status === "pending";
-    return true; 
-  });
+  const hasTasks = context.tasks.length > 0;
 
   return (
     <div>
       <h1>My Day</h1>
       <p>All my tasks in one place</p>
-      <div>
-        <CrearTarea /> 
-        {filteredTasks.map((task) => (
-          <Tarea key={task.id} task={task} /> 
-        ))}
-      </div>
-      <Footer />
+      
+      <CrearTarea /> 
+
+      {hasTasks && (
+        <div id="main">
+          {context.tasks.map((task) => (
+            <Tarea key={task.id} task={task} />
+          ))}
+        </div>
+      )}
+
+      {hasTasks && <Footer />}
     </div>
   );
 }
